5fd817a458a1b581780f0cd246e88a5dc05f3633
softprops <d.tangren@gmail.com>
2020-06-12 21:47:59 -0400

    fmt

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,12 @@ use structopt::StructOpt;
 async fn main() -> anyhow::Result<()> {
     let args = Push::from_args();
     push::run(
-                env::var("DEVTO_API_KEY")
-                    .map_err(|_| anyhow::anyhow!(
-                        "Please export a DEVTO_API_KEY env variable..\n  ▶ You can generate one by visiting https://dev.to/settings/account"
-                    ))?,
-                args,
-            )
-            .await?;
+        env::var("DEVTO_API_KEY")
+            .map_err(|_| anyhow::anyhow!(
+                "Please export a DEVTO_API_KEY env variable..\n  ▶ You can generate one by visiting https://dev.to/settings/account"
+            ))?,
+        args,
+    )
+    .await?;
     Ok(())
 }
